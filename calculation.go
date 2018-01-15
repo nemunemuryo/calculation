2b43a54ff319c3f54ea97f898b240a74947f6520
nemunemu <e155719@ie.u-ryukyu.ac.jp>
2018-01-15 20:00:50 +0900

    add goparsec

--- calculation.go
+++ calculation.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+  "flag"
+  "fmt"
   "io"
-  "strings"
-  "strconv"
+  "io/ioutil"
+  "os"
+  // "strings"
+  // "strconv"
   "github.com/chzyer/readline"
+  "github.com/prataprc/goparsec"
+  "github.com/prataprc/goparsec/expr"
 )
+
+var options struct {
+  expr string
+}
+
+func argParse() {
+  flag.StringVar(&options.expr, "expr", "",
+  "Specify input file or arithmetic expression string")
+  flag.Parse()
+}
 
 func main() {
   l, err := readline.NewEx(&readline.Config{
@@ -31,23 +47,45 @@ func main() {
     }
 
     //splitと数値変換
-    word := strings.Split(line, " ")
-    var i int
-    var j int
-    i, _ = strconv.Atoi(word[0])
-    j, _ = strconv.Atoi(word[2])
+    // word := strings.Split(line, " ")
+    // var i int
+    // var j int
+    // i, _ = strconv.Atoi(word[0])
+    // j, _ = strconv.Atoi(word[2])
 
-    //演算
-    switch word[1] {
-    case "+":
-      println(i + j)
-    case "-":
-      println(i - j)
-    case "*":
-      println(i * j)
-    case "/":
-      println(i / j)
+    argParse()
+    if options.expr != "" {
+      doExpr(getText(options.expr))
     }
+    //演算
+
+    // switch word[1] {
+    // case "+":
+    //   println(i + j)
+    // case "-":
+    //   println(i - j)
+    // case "*":
+    //   println(i * j)
+    // case "/":
+    //   println(i / j)
+    // }
   }
 
+}
+
+func doExpr(text string) {
+  s := parsec.NewScanner([]byte(text))
+  v, _ := expr.Y(s)
+  fmt.Println(v)
+}
+
+func getText(filename string) string {
+  if _, err := os.Stat(filename); err != nil {
+    return filename
+  }
+  if b, err := ioutil.ReadFile(filename); err != nil {
+    panic(err)
+  } else {
+    return string(b)
+  }
 }
