29bbc00c1822d60b47d7ccf9c635c54782d397d7
itoooo <13117917+itoooo@users.noreply.github.com>
2018-01-18 14:54:54 +0900

    Fix evaluation fail.

--- calculation.go
+++ calculation.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-  "fmt"
-  "io"
-  //"strconv"
-  "flag"
-  "io/ioutil"
-  "os"
-  //"strings"
-  "github.com/chzyer/readline"
-  "github.com/prataprc/goparsec"
-  "github.com/prataprc/goparsec/expr"
+	"fmt"
+	"io"
+	//"strconv"
+	"flag"
+	"io/ioutil"
+	"os"
+	//"strings"
+	"github.com/chzyer/readline"
+	"github.com/prataprc/goparsec"
+	"github.com/prataprc/goparsec/expr"
 )
 
 // goparsec/exprがあるし使えば関係はず...--------------------------------------------------------
@@ -138,90 +138,88 @@ import (
 //----------------------------------------------------------------------------------------------
 
 var options struct {
-  expr string
+	expr string
 }
 
 func argParse() {
-  // flag.StringVar(&options.expr, "expr", "",
-  // "Specify input file or arithmetic expression string")
-  // flag.Parse()
+	// flag.StringVar(&options.expr, "expr", "",
+	// "Specify input file or arithmetic expression string")
+	// flag.Parse()
 
-  // 引数の間に余計なものが合ってもフラグ解析が切れることはない実装
-  f := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-  f.StringVar(&options.expr, "expr", "",
-  "Specify input file or arithmetic expression string")
+	// 引数の間に余計なものが合ってもフラグ解析が切れることはない実装
+	f := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	f.StringVar(&options.expr, "expr", "",
+		"Specify input file or arithmetic expression string")
 
 	f.Parse(os.Args[1:])
 	for 0 < f.NArg() {
 		f.Parse(f.Args()[1:])
-  }
+	}
 }
 
 func main() {
-  l, err := readline.NewEx(&readline.Config{
-    Prompt:          "\033[31m»\033[0m ",
-    InterruptPrompt: "^C",
-    EOFPrompt:       "exit",
-  })
-  if err != nil {
-    panic(err)
-  }
-  defer l.Close()
+	l, err := readline.NewEx(&readline.Config{
+		Prompt:          "\033[31m»\033[0m ",
+		InterruptPrompt: "^C",
+		EOFPrompt:       "exit",
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
 
-  for {
-    line, err := l.Readline()
-    if err == readline.ErrInterrupt {
-      if len(line) == 0 {
-        break
-      } else {
-        continue
-      }
-    } else if err == io.EOF {
-      break
-    }
+	for {
+		line, err := l.Readline()
+		if err == readline.ErrInterrupt {
+			if len(line) == 0 {
+				break
+			}
+		} else if err == io.EOF {
+			break
+		}
 
-    options.expr = line
-    println(options.expr)
-    argParse()
-    if options.expr != "" {
-      doExpr(getText(options.expr))
-    }
+		options.expr = line
+		println(options.expr)
+		argParse()
+		if options.expr != "" {
+			doExpr(getText(options.expr))
+		}
 
-    //splitと数値変換
-    // word := strings.Split(line, " ")
-    // var i int
-    // var j int
-    // i, _ = strconv.Atoi(word[0])
-    // j, _ = strconv.Atoi(word[2])
+		//splitと数値変換
+		// word := strings.Split(line, " ")
+		// var i int
+		// var j int
+		// i, _ = strconv.Atoi(word[0])
+		// j, _ = strconv.Atoi(word[2])
 
-    //演算
-    // switch word[1] {
-    // case "+":
-    //   println(i + j)
-    // case "-":
-    //   println(i - j)
-    // case "*":
-    //   println(i * j)
-    // case "/":
-    //   println(i / j)
-    // }
-  }
+		//演算
+		// switch word[1] {
+		// case "+":
+		//   println(i + j)
+		// case "-":
+		//   println(i - j)
+		// case "*":
+		//   println(i * j)
+		// case "/":
+		//   println(i / j)
+		// }
+	}
 
 }
 
 func doExpr(text string) {
-  s := parsec.NewScanner([]byte(text))
-  v, _ := expr.Y(s)
-  fmt.Println(v)
+	s := parsec.NewScanner([]byte(text))
+	v, _ := expr.Y(s)
+	fmt.Println(v)
 }
 
 func getText(filename string) string {
-  if _, err := os.Stat(filename); err != nil {
-    return filename
-  }
-  if b, err := ioutil.ReadFile(filename); err != nil {
-    panic(err)
-  } else {
-    return string(b)
-  }
+	if _, err := os.Stat(filename); err != nil {
+		return filename
+	}
+	if b, err := ioutil.ReadFile(filename); err != nil {
+		panic(err)
+	} else {
+		return string(b)
+	}
 }
